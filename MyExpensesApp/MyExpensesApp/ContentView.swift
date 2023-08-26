24028c87895035fa46cffcd65f3ac450acbd39aa
Arkadiusz Młynarczyk <arkadiusz.mlyn@icloud.com>
2023-08-26 22:45:23 +0200

    first steps

---
+++ MyExpensesApp/MyExpensesApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  MyExpensesApp
+//
+//  Created by Arkadiusz Młynarczyk on 26/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyExpensesApp/MyExpensesApp/MyExpensesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyExpensesAppApp.swift
+//  MyExpensesApp
+//
+//  Created by Arkadiusz Młynarczyk on 26/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyExpensesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
